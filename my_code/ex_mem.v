88a9e11099e9da29e00bf9dd8d44fad9ea5a263b
szy <18829215696@163.com>
2024-02-19 22:20:13 +0800

    ex_mem——ex/mem模块编写——24.2.19

---
+++ my_code/ex_mem.v
@@ -0,0 +1,29 @@
+`include "defines.v"
+
+module ex_mem (
+    input wire clk,
+    input wire rst,
+    input wire[`RegAddrBus] ex_wd,
+    input wire[`RegBus] ex_wdata,
+    input wire ex_wreg,
+
+    output reg[`RegAddrBus] mem_wd,
+    output reg[`RegBus] mem_wdata,
+    output reg mem_wreg
+);
+    always @(posedge clk ) begin
+        if (rst==`RstEnable) begin
+            mem_wd<=`NOPRegAddr;
+            mem_wdata<=`ZeroWord;
+            mem_wreg<=`WriteDisable;
+        end else begin
+            mem_wd<=ex_wd;
+            mem_wdata<=ex_wdata;
+            mem_wreg<=ex_wreg;
+        end
+    end
+
+
+
+
+endmodule
