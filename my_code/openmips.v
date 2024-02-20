44894076c51dc2b3c3a94089eacf23141f1846f6
szy <18829215696@163.com>
2024-02-20 11:58:51 +0800

    openmips.v——顶层模块编写完成——24.2.20

--- my_code/openmips.v
+++ my_code/openmips.v
@@ -56,6 +56,115 @@ wire reg2_read;
 
 assign rom_addr_o = pc;
 
+pc_reg u_pc_reg(
+    .clk ( clk ),
+    .rst ( rst ),
+    .pc  ( pc  ),
+    .ce  ( rom_ce_o  )
+);
+
+if_id u_if_id(
+    .clk     ( clk     ),
+    .rst     ( rst     ),
+    .if_pc   ( pc   ),
+    .if_inst ( rom_data_i ),
+    .id_pc   ( id_pc_i   ),
+    .id_inst  ( id_inst_i  )
+);
+
+id u_id(
+    .rst         ( rst         ),
+    .pc_i        ( id_pc_i        ),
+    .inst_i      ( id_inst_i      ),
+    .reg1_data_i ( reg1_data ),
+    .reg2_data_i ( reg2_data ),
+    .reg1_read_o ( reg1_read ),
+    .reg2_read_o ( reg2_read ),
+    .reg1_addr_o ( reg1_addr ),
+    .reg2_addr_o ( reg2_addr ),
+    .aluop_o     ( id_aluop_o     ),
+    .alusel_o    ( id_alusel_o    ),
+    .reg1_o      ( id_reg1_o      ),
+    .reg2_o      ( id_reg2_o      ),
+    .wd_o        ( id_wd_o        ),
+    .wreg_o      ( id_wreg_o      )
+);
+
+id_ex u_id_ex(
+    .clk       ( clk       ),
+    .rst       ( rst       ),
+    .id_aluop  ( id_aluop_o  ),
+    .id_alusel ( id_alusel_o ),
+    .id_reg1   ( id_reg1_o   ),
+    .id_reg2   ( id_reg2_o   ),
+    .id_wd     ( id_wd_o     ),
+    .id_wreg   ( id_wreg_o   ),
+    .ex_aluop  ( ex_aluop_i  ),
+    .ex_alusel ( ex_alusel_i ),
+    .ex_reg1   ( ex_reg1_i   ),
+    .ex_reg2   ( ex_reg2_i   ),
+    .ex_wd     ( ex_wd_i     ),
+    .ex_wreg   ( ex_wreg_i   )
+);
+
+ex u_ex(
+    .rst      ( rst      ),
+    .alusel_i ( ex_alusel_i ),
+    .aluop_i  ( ex_aluop_i  ),
+    .reg1_i   ( ex_reg1_i   ),
+    .reg2_i   ( ex_reg2_i   ),
+    .wd_i     ( ex_wd_i     ),
+    .wreg_i   ( ex_wreg_i   ),
+    .wd_o     ( ex_wd_o     ),
+    .wreg_o   ( ex_wreg_o   ),
+    .wdata_o  ( ex_wdata_o  )
+);
+
+ex_mem u_ex_mem(
+    .clk       ( clk       ),
+    .rst       ( rst       ),
+    .ex_wd     ( ex_wd_o     ),
+    .ex_wdata  ( ex_wdata_o  ),
+    .ex_wreg   ( ex_wreg_o   ),
+    .mem_wd    ( mem_wd_i    ),
+    .mem_wdata ( mem_wdata_i ),
+    .mem_wreg  ( mem_wreg_i  )
+);
+
+mem u_mem(
+    .rst     ( rst     ),
+    .wd_i    ( mem_wd_i    ),
+    .wreg_i  ( mem_wreg_i  ),
+    .wdata_i ( mem_wdata_i ),
+    .wd_o    ( mem_wd_o    ),
+    .wdata_o ( mem_wdata_o ),
+    .wreg_o  ( mem_wreg_o  )
+);
+
+mem_wb u_mem_wb(
+    .clk       ( clk       ),
+    .rst       ( rst       ),
+    .mem_wd    ( mem_wd_o    ),
+    .mem_wreg  ( mem_wreg_o  ),
+    .mem_wdata ( mem_wdata_o ),
+    .wb_wd     ( wb_wd_i     ),
+    .wb_wreg   ( wb_wreg_i   ),
+    .wb_wdata  ( wb_wdata_i  )
+);
+
+regfile u_regfile(
+    .clk    ( clk    ),
+    .rst    ( rst    ),
+    .we     ( wb_wreg_i     ),
+    .waddr  ( wb_wd_i  ),
+    .wdata  ( wb_wdata_i  ),
+    .re1    ( reg1_read    ),
+    .raddr1 ( reg1_addr ),
+    .rdata1 ( reg1_data),
+    .re2    ( reg2_read ),
+    .raddr2 ( reg2_addr),
+    .rdata2 ( reg2_data)
+);
 
 
 endmodule
