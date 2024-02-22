8515821a5e250d159255372c275e16c3772a10ad
szy <18829215696@163.com>
2024-02-22 20:18:18 +0800

    defines.v——增加chapter5的指令宏定义——24.2.22

--- my_code/defines.v
+++ my_code/defines.v
@@ -27,8 +27,25 @@
 `define TrapNotAssert        1'b0                        //
 
 //***************************与具体指令有关的宏定义******************************//
+`define EXE_AND              6'b100100                        //指令and的指令码
+`define EXE_OR               6'b100101                        //指令or的指令码
+`define EXE_XOR              6'b100110                        //指令xor的指令码
+`define EXE_NOR              6'b100111                        //指令nor的指令码
+`define EXE_ANDI             6'b001100                        //指令andi的指令码
 `define EXE_ORI              6'b001101                        //指令ori的指令码
-`define EXE_NOP              6'b000000                        //
+`define EXE_XORI             6'b001110                        //指令xori的指令码
+`define EXE_LUI              6'b001111                        //指令lui的指令码
+
+`define EXE_SLL              6'b000000                        //指令sll的指令码
+`define EXE_SLLV             6'b001000                        //指令srl的指令码
+`define EXE_SRL              6'b000010                        //指令srl的指令码
+`define EXE_SRLV             6'b000110                        //指令srlv的指令码
+`define EXE_SRA              6'b000011                        //指令sra的指令码
+`define EXE_SRAV             6'b000111                        //指令srav的指令码
+
+`define EXE_SYNC             6'b001111                        //指令sync的指令码
+`define EXE_PREF             6'b110011                        //指令pref的指令码
+`define EXE_SPECIAL_INST     6'b000000                        //special类指令的指令码
 
 
 //AluOp
