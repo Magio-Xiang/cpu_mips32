3c538a751e999e6379481376fc0e33eeaca0cc77
szy <18829215696@163.com>
2024-03-04 20:22:51 +0800

    pc_reg.v——取指阶段增加暂停——24.3.4

--- my_code/pc_reg.v
+++ my_code/pc_reg.v
@@ -2,6 +2,7 @@
 module pc_reg (
     input wire clk,
     input wire rst,
+    input wire[5:0] stall,
 
     output reg[`InstAddrBus] pc,
     output reg ce 
@@ -22,10 +23,11 @@ module pc_reg (
         begin
             pc<=`ZeroWord;
         end
-        else
+        else if(stall[0]==`NoStop)
         begin
             pc<=pc+4'h4;
         end
+        
 
     end
 endmodule
