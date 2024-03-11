897b66fc5216cd029cd019431ab8814b4b0c6558
szy <18829215696@163.com>
2024-03-11 23:01:32 +0800

    pc_reg.v——增加转移指令处理——24.3.11

--- my_code/pc_reg.v
+++ my_code/pc_reg.v
@@ -3,6 +3,8 @@ module pc_reg (
     input wire clk,
     input wire rst,
     input wire[5:0] stall,
+    input wire branch_flag_i,
+    input wire[`RegBus] branch_target_address_i,
 
     output reg[`InstAddrBus] pc,
     output reg ce 
@@ -25,7 +27,12 @@ module pc_reg (
         end
         else if(stall[0]==`NoStop)
         begin
-            pc<=pc+4'h4;
+            if (branch_flag_i == `Branch) begin
+                pc<=branch_target_address_i;
+            end else begin
+                pc<=pc+4'h4;
+            end
+
         end
         
 
