957658e53aadc5d957efbef9c71cd4faedfaf574
szy <18829215696@163.com>
2024-03-15 18:48:52 +0800

    openmips_min_sopc.v——增加简单加载存储指令处理——24.3.15

--- my_code/openmips_min_sopc.v
+++ my_code/openmips_min_sopc.v
@@ -8,6 +8,13 @@ module openmips_min_sopc (
     wire[`InstBus] inst;
     wire rom_ce;
 
+    wire[`DataBus] ram_data_i;
+    wire[`DataBus] ram_data_o;
+    wire[`DataAddrBus] ram_addr;
+    wire[3:0] ram_sel;
+    wire ram_we;
+    wire ram_ce;
+
 
     inst_rom u_inst_rom(
         .ce   ( rom_ce   ),
@@ -20,8 +27,25 @@ module openmips_min_sopc (
         .clk        ( clk        ),
         .rom_data_i ( inst ),
         .rom_addr_o ( inst_addr ),
-        .rom_ce_o   ( rom_ce   )
+        .rom_ce_o   ( rom_ce   ),
+
+        .ram_data_i(ram_data_i),
+        .ram_data_o(ram_data_o),
+        .ram_addr_o(ram_addr),
+        .ram_sel_o(ram_sel),
+        .ram_we_o(ram_we),
+        .ram_ce_o(ram_ce)
     );
+
+    data_ram u_data_ram(
+    .ce     ( ram_ce     ),
+    .clk    ( clk    ),
+    .data_i ( ram_data_o ),
+    .addr   ( ram_addr  ),
+    .we     ( ram_we     ),
+    .sel    ( ram_sel ),
+    .data_o  ( ram_data_i  )
+);
 
 
 endmodule
