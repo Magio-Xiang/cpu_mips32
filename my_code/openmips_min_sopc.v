f8dc55f9560848f4096fb67cccfc345398ec7817
szy <18829215696@163.com>
2024-03-19 20:04:43 +0800

    openmips_min_sopc.v——增加协处理器cp0处理——24.3.19

--- my_code/openmips_min_sopc.v
+++ my_code/openmips_min_sopc.v
@@ -15,6 +15,10 @@ module openmips_min_sopc (
     wire ram_we;
     wire ram_ce;
 
+    wire[5:0] int;
+    wire timer_int;
+    //assign int = {5'b00000, timer_int, gpio_int, uart_int};
+    assign int = {5'b00000, timer_int};
 
     inst_rom u_inst_rom(
         .ce   ( rom_ce   ),
@@ -28,13 +32,15 @@ module openmips_min_sopc (
         .rom_data_i ( inst ),
         .rom_addr_o ( inst_addr ),
         .rom_ce_o   ( rom_ce   ),
+        .int_i      (int),
 
         .ram_data_i(ram_data_i),
         .ram_data_o(ram_data_o),
         .ram_addr_o(ram_addr),
         .ram_sel_o(ram_sel),
         .ram_we_o(ram_we),
-        .ram_ce_o(ram_ce)
+        .ram_ce_o(ram_ce),
+        .timer_int_o(timer_int)
     );
 
     data_ram u_data_ram(
