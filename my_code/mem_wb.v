99a7ff1dd43b3cc42c7002917faab9f3a8aa78b5
szy <18829215696@163.com>
2024-03-19 20:03:49 +0800

    mem_wb.v——增加协处理器cp0处理——24.3.19

--- my_code/mem_wb.v
+++ my_code/mem_wb.v
@@ -19,6 +19,10 @@ module mem_wb(
 	input wire mem_LLbit_we,
 	input wire mem_LLbit_value,
 
+	input wire              mem_cp0_reg_we,
+    input wire[`RegBus]     mem_cp0_reg_data,
+    input wire[`RegAddrBus] mem_cp0_reg_write_addr,
+
 	//送到回写阶段的信息
 	output reg[`RegAddrBus]      wb_wd,
 	output reg                   wb_wreg,
@@ -28,7 +32,11 @@ module mem_wb(
 	output reg[`RegBus] wb_hi,
 	output reg[`RegBus] wb_lo,
 	output reg wb_LLbit_we,
-	output reg wb_LLbit_value	       
+	output reg wb_LLbit_value,
+
+    output reg              wb_cp0_reg_we,
+    output reg[`RegBus]     wb_cp0_reg_data,
+    output reg[`RegAddrBus] wb_cp0_reg_write_addr	       
 	
 
 );
@@ -44,6 +52,9 @@ module mem_wb(
 			wb_lo<=`ZeroWord;
 			wb_LLbit_we<=`WriteDisable;
 			wb_LLbit_value<=1'b0;
+            wb_cp0_reg_we<=`WriteDisable;
+            wb_cp0_reg_data<=`ZeroWord;
+            wb_cp0_reg_write_addr<=5'b00000;
 		end else if(stall[4]==`Stop && stall[5]==`NoStop) begin
 			wb_wd <= `NOPRegAddr;
 			wb_wreg <= `WriteDisable;
@@ -53,6 +64,9 @@ module mem_wb(
 			wb_lo<=`ZeroWord;
 			wb_LLbit_we<=`WriteDisable;
 			wb_LLbit_value<=1'b0;
+			wb_cp0_reg_we<=`WriteDisable;
+            wb_cp0_reg_data<=`ZeroWord;
+            wb_cp0_reg_write_addr<=5'b00000;
 		end else if(stall[4]==`NoStop) begin
 			wb_wd <= mem_wd;
 			wb_wreg <= mem_wreg;
@@ -62,6 +76,9 @@ module mem_wb(
 			wb_lo<=mem_lo;
 			wb_LLbit_we<=mem_LLbit_we;
 			wb_LLbit_value<=mem_LLbit_value;
+			wb_cp0_reg_we<=mem_cp0_reg_we;
+            wb_cp0_reg_data<=mem_cp0_reg_data;
+            wb_cp0_reg_write_addr<=mem_cp0_reg_write_addr;
 		end    //if
 	end      //always
 			
