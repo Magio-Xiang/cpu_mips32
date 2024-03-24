3687a307ea8f896591a1960abc2d83c5d60b677c
szy <18829215696@163.com>
2024-03-24 22:26:56 +0800

    id_ex.v——异常相关修改——24.3.24

--- my_code/id_ex.v
+++ my_code/id_ex.v
@@ -17,6 +17,10 @@ module id_ex (
   input wire id_is_in_delayslot,
   input wire next_inst_in_delayslot_i,
   input wire[`RegBus] id_inst,
+  
+  input wire flush,
+  input wire[31:0] id_excepttype,
+  input wire[`RegBus] id_current_inst_address,
 
   output reg[`AluOpBus] ex_aluop,
   output reg[`AluSelBus] ex_alusel,
@@ -29,7 +33,10 @@ module id_ex (
   output reg[`RegBus] ex_link_address,
   output reg ex_is_in_delayslot,
   output reg is_in_delayslot_o,
-  output reg[`RegBus] ex_inst
+  output reg[`RegBus] ex_inst,
+
+  output reg[31:0] ex_excepttype,
+  output reg[`RegBus] ex_current_inst_address
 
 );
     always @(posedge clk) begin
@@ -44,6 +51,21 @@ module id_ex (
             ex_is_in_delayslot<=`NotInDelaySlot;
             is_in_delayslot_o<=`NotInDelaySlot;
             ex_inst<=`ZeroWord;
+            ex_excepttype<=`ZeroWord;
+            ex_current_inst_address<=`ZeroWord;
+        end else if (flush == 1'b1) begin
+            ex_aluop<=`EXE_NOP_OP;
+            ex_alusel<=`EXE_RES_NOP;
+            ex_reg1<=`ZeroWord;
+            ex_reg2<=`ZeroWord;
+            ex_wd<=`NOPRegAddr;
+            ex_wreg<=`WriteDisable;
+            ex_link_address<=`ZeroWord;
+            ex_is_in_delayslot<=`NotInDelaySlot;
+            is_in_delayslot_o<=`NotInDelaySlot;
+            ex_inst<=`ZeroWord;
+            ex_excepttype<=`ZeroWord;
+            ex_current_inst_address<=`ZeroWord;
         end else if (stall[2]==`Stop && stall[3]==`NoStop) begin
             ex_aluop<=`EXE_NOP_OP;
             ex_alusel<=`EXE_RES_NOP;
@@ -54,6 +76,8 @@ module id_ex (
             ex_link_address<=`ZeroWord;
             ex_is_in_delayslot<=`NotInDelaySlot;
             ex_inst<=`ZeroWord;
+            ex_excepttype<=`ZeroWord;
+            ex_current_inst_address<=`ZeroWord;
         end else if(stall[2]==`NoStop)  begin
             ex_aluop<=id_aluop;
             ex_alusel<=id_alusel;
@@ -65,6 +89,8 @@ module id_ex (
             ex_is_in_delayslot<=id_is_in_delayslot;
             is_in_delayslot_o<=next_inst_in_delayslot_i;
             ex_inst<=id_inst;
+            ex_excepttype<=id_excepttype;
+            ex_current_inst_address<=id_current_inst_address;
         end
     end
 
