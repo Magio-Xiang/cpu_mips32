7b02665e5c503c80a65a83b8f3e104f29a478a92
szy <18829215696@163.com>
2024-03-24 22:27:26 +0800

    mem.v——异常相关修改——24.3.24

--- my_code/mem.v
+++ my_code/mem.v
@@ -22,6 +22,18 @@ module mem (
     input wire[`RegAddrBus] cp0_reg_write_addr_i,
     input wire[`RegBus] cp0_reg_data_i,	
 
+    input wire[31:0] excepttype_i,
+    input wire is_in_delayslot_i,
+    input wire[`RegBus] current_inst_address_i,
+
+    input wire[`RegBus] cp0_status_i,
+    input wire[`RegBus] cp0_cause_i,
+    input wire[`RegBus] cp0_epc_i,
+
+    input wire wb_cp0_reg_we,
+    input wire[4:0] wb_cp0_reg_write_addr,
+    input wire[`RegBus] wb_cp0_reg_data,
+
     output reg[`RegAddrBus] wd_o,
     output reg[`RegBus] wdata_o,
     output reg wreg_o,
@@ -40,15 +52,28 @@ module mem (
 
     output reg cp0_reg_we_o,
     output reg[`RegAddrBus] cp0_reg_write_addr_o,
-    output reg[`RegBus] cp0_reg_data_o	
+    output reg[`RegBus] cp0_reg_data_o,
+
+    output reg[3:0] excepttype_o,
+    output wire[`RegBus] cp0_epc_o,
+    output wire is_in_delayslot_o,
+
+    output wire[`RegBus] current_inst_address_o	
 );
     reg LLbit;
 
     wire[`RegBus] zero32;
     reg mem_we;
+    reg[`RegBus] cp0_status;
+    reg[`RegBus] cp0_cause;
+    reg[`RegBus] cp0_epc;
+
 
     assign mem_we_o=mem_we;
     assign zero32 = `ZeroWord;
+    assign is_in_delayslot_o=is_in_delayslot_i;
+    assign current_inst_address_o=current_inst_address_i;
+    assign mem_we_o = mem_we & (~(|excepttype_o));
 
     always @(*) begin
         if (rst==`RstEnable) begin
@@ -61,6 +86,68 @@ module mem (
             end
         end
     end
+
+ 	always @ (*) begin
+		if(rst == `RstEnable) begin
+			cp0_status <= `ZeroWord;
+		end else if((wb_cp0_reg_we == `WriteEnable) && 
+								(wb_cp0_reg_write_addr == `CP0_REG_STATUS ))begin
+			cp0_status <= wb_cp0_reg_data;
+		end else begin
+		  cp0_status <= cp0_status_i;
+		end
+	end
+	
+	always @ (*) begin
+		if(rst == `RstEnable) begin
+			cp0_epc <= `ZeroWord;
+		end else if((wb_cp0_reg_we == `WriteEnable) && 
+								(wb_cp0_reg_write_addr == `CP0_REG_EPC ))begin
+			cp0_epc <= wb_cp0_reg_data;
+		end else begin
+		  cp0_epc <= cp0_epc_i;
+		end
+	end
+
+    always @ (*) begin
+		if(rst == `RstEnable) begin
+			cp0_cause <= `ZeroWord;
+		end else if((wb_cp0_reg_we == `WriteEnable) && 
+								(wb_cp0_reg_write_addr == `CP0_REG_CAUSE ))begin
+			cp0_cause[9:8] <= wb_cp0_reg_data[9:8];
+			cp0_cause[22] <= wb_cp0_reg_data[22];
+			cp0_cause[23] <= wb_cp0_reg_data[23];
+		end else begin
+		  cp0_cause <= cp0_cause_i;
+		end
+	end
+
+    always @ (*) begin
+		if(rst == `RstEnable) begin
+			excepttype_o <= `ZeroWord;
+		end else begin
+			excepttype_o <= `ZeroWord;
+			
+			if(current_inst_address_i != `ZeroWord) begin
+				if(((cp0_cause[15:8] & (cp0_status[15:8])) != 8'h00) && (cp0_status[1] == 1'b0) && 
+							(cp0_status[0] == 1'b1)) begin
+					excepttype_o <= 32'h00000001;        //interrupt
+				end else if(excepttype_i[8] == 1'b1) begin
+			  	excepttype_o <= 32'h00000008;        //syscall
+				end else if(excepttype_i[9] == 1'b1) begin
+					excepttype_o <= 32'h0000000a;        //inst_invalid
+				end else if(excepttype_i[10] ==1'b1) begin
+					excepttype_o <= 32'h0000000d;        //trap
+				end else if(excepttype_i[11] == 1'b1) begin  //ov
+					excepttype_o <= 32'h0000000c;
+				end else if(excepttype_i[12] == 1'b1) begin  //返回指令
+					excepttype_o <= 32'h0000000e;
+				end
+			end
+				
+		end
+	end			
+
 
     always @(*) begin
         if (rst==`RstEnable) begin
